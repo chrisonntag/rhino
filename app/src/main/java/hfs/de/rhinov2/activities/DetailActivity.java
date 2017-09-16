607ec888bf70a20b54625fcdeb200892748272a5
Tobias Hilbig <web.tobias@hilbig-ffb.de>
2017-09-16 21:06:31 +0200

    Adds detail view activity stub

---
+++ app/src/main/java/hfs/de/rhinov2/activities/DetailActivity.java
@@ -0,0 +1,15 @@
+package hfs.de.rhinov2.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hfs.de.rhinov2.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
