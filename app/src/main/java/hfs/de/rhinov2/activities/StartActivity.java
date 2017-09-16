1750fbfaf92e210ede7c71cd229c0e1df92d35d6
Tobias Hilbig <web.tobias@hilbig-ffb.de>
2017-09-17 01:29:45 +0200

    dito

--- app/src/main/java/hfs/de/rhinov2/activities/StartActivity.java
+++ app/src/main/java/hfs/de/rhinov2/activities/StartActivity.java
@@ -33,6 +33,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        preferences = getSharedPreferences(prefpath, Context.MODE_PRIVATE);
 
         if (preferences.contains("City") && preferences.contains("Latitude") &&preferences.contains("Longitude")) {
             store.setCity(preferences.getString("City", null));
@@ -77,7 +78,6 @@ public class StartActivity extends AppCompatActivity {
             }
         });*/
 
-            preferences = getSharedPreferences(prefpath, Context.MODE_PRIVATE);
             set = (Button) findViewById(R.id.set);
             set.setOnClickListener(new View.OnClickListener() {
                 @Override
