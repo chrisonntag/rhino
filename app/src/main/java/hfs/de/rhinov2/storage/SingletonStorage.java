2f861cb28ed521af9735d661f983a267cd6f6665
Tobias Hilbig <web.tobias@hilbig-ffb.de>
2017-09-16 22:19:29 +0200

    Implements SingletonStorage

--- app/src/main/java/hfs/de/rhinov2/storage/SingletonStorage.java
+++ app/src/main/java/hfs/de/rhinov2/storage/SingletonStorage.java
@@ -10,25 +10,26 @@ import lombok.Setter;
 /**
  * Created by anselm on 16.09.17.
  */
-@Getter
+
 @Setter
+@Getter
 public class SingletonStorage {
     // coords
-    private static LatLng coordinates;
+    private LatLng coordinates;
 
     // threat data
-    private static String threatArea;
-    private static List<String> threatCategories;
-    private static String threatDesc;
-    private static String threatHeadline;
-    private static List<String> threatInstructions;
-    private static String threatType;
-    private static String threatSeverity;
-    private static String threatSource;
+    private String threatArea;
+    private List<String> threatCategories;
+    private String threatDesc;
+    private String threatHeadline;
+    private List<String> threatInstructions;
+    private String threatType;
+    private String threatSeverity;
+    private String threatSource;
 
     private static final SingletonStorage storageInstance = new SingletonStorage();
 
-    public static SingletonStorage getInstance(){
+    public static SingletonStorage getInstance() {
         return storageInstance;
     }
 }
